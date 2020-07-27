03e474bae1658efa47576053529f03abfec44b94
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-27 07:16:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,8 @@ def start_game(player1, player2)
  puts "Hello #{player1} & #{player2}"
 end
 
-
+player1 = "Jay-Z"
+player2 = "Beyonce"
 def play_game(player2)
   puts "#{player1} is better than #{player2}"
 end
